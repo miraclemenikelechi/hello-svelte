70e2f79505efde53e658b70ccd449a26a40f4157
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-01-19 18:32:14 +0100

    hello svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+
+	type tStatus = 'OPEN' | 'CLOSE';
+
+	let name: string = $state('trinity');
+
+	let status: tStatus = $state('OPEN');
+
+	function toggle() {
+		status = status === 'OPEN' ? 'CLOSE' : 'OPEN';
+	}
+</script>
+
+<div class="text-red-800">hello world</div>
+
+<Header {name} />
+
+<input type="text" bind:value={name} />
+
+<p>the store is now {status}</p>
+<button onclick={toggle}>toggle status</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,5 @@
+<script>
+	let { name } = $props();
+</script>
+
+<h1>hello from {name}</h1>
